39eed407ab51cebcfc6104f0cac0123e94a5528e
Ben Saunders <ben@kaizeninternet.com>
2022-10-16 21:39:29 +0100

    Extract 'counts' and 'visits' methods

--- log_parser.rb
+++ log_parser.rb
@@ -4,32 +4,42 @@
 class LogParser
   def initialize(log_file_location)
     @log_file = File.new(log_file_location)
-    @visits = []
-    @log_file.each do |line|
-      @visits << {
-        path: line.split(' ').first,
-        ip: line.split(' ').last
-      }
-    end
-    @counts = {}
-    @visits.each do | visit |
-      @counts[visit[:path]] ||= {
+  end
+
+  def counts
+    return @counts if @counts
+    counts = {}
+    visits.each do | visit |
+      counts[visit[:path]] ||= {
         visits: 0,
         unique_visits: 0,
         ips: []
       }
-      @counts[visit[:path]][:visits] += 1
-      @counts[visit[:path]][:ips] |= [visit[:ip]]
-      @counts[visit[:path]][:unique_visits] = @counts[visit[:path]][:ips].size
+      counts[visit[:path]][:visits] += 1
+      counts[visit[:path]][:ips] |= [visit[:ip]]
+      counts[visit[:path]][:unique_visits] = counts[visit[:path]][:ips].size
     end
+    @counts = counts
+  end
+
+  def visits
+    return @visits if @visits
+    visits = []
+    @log_file.each do |line|
+      visits << {
+        path: line.split(' ').first,
+        ip: line.split(' ').last
+      }
+    end
+    @visits = visits
   end
 
   def pages_by_visits
-    @counts.sort_by{| k, v | v[:visits] }.reverse.map{|k, v| "#{k} #{v[:visits]}"}
+    counts.sort_by{| k, v | v[:visits] }.reverse.map{|k, v| "#{k} #{v[:visits]}"}
   end
 
   def pages_by_unique_visits
-    @counts.sort_by{| k, v | v[:unique_visits] }.reverse.map{|k, v| "#{k} #{v[:unique_visits]}"}
+    counts.sort_by{| k, v | v[:unique_visits] }.reverse.map{|k, v| "#{k} #{v[:unique_visits]}"}
   end
 
   def report
